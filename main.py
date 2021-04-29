435d5a3d9bb1a1869907b373566a7ab4e055b827
JorikL <83394320+JorikL@users.noreply.github.com>
2021-04-29 11:53:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import curses, os, sys
+
+if len(sys.argv) is 1 and os.path.isfile('input.txt.hacker'):
+    src_file = open('input.txt.hacker')
+elif len(sys.argv) is 2 and os.path.isfile(sys.argv[1]):
+    src_file = open(sys.argv[1])
+else:
+    print("error de file input.txt.hacker kan ik niet vinden") #error
+    exit(1)
+
+myscreen = curses.initscr()
+curses.start_color()
+curses.noecho()
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+char = 0
+
+myscreen.getch()
+
+for line in src_file:
+    if char == 27:
+        break
+
+
+    for word in line:
+        myscreen.addstr(word , curses.color_pair(1))
+        myscreen.refresh()
+        char = myscreen.getch()
+        if char == 27:
+            break
+
+    myscreen.refresh()
+
+if char == 27:
+    curses.endwin()
+else:
+    while char != 27:
+        char = myscreen.getch()
+
+    curses.endwin()
